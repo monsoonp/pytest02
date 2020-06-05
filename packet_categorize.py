972ac9dfb63a258a0f20f64395977cddd234ea12
monsoonp <alstn0706@naver.com>
2020-06-05 15:41:33 +0900

    mms packet to redis / expire

--- packet_categorize.py
+++ packet_categorize.py
@@ -1,6 +1,6 @@
 import time
 import re
-from datetime import datetime
+from datetime import datetime, timedelta
 import pyshark
 import redis
 import json
@@ -20,6 +20,45 @@ class Dictlist(dict):  # dictionary value 가 중복일 시 배열로 추가함
         self[key].append(value)
 
 
+class Report(dict):
+    def __init__(self, redis_conn):
+        self.r = redis_conn
+
+    def __setitem__(self, key, value):
+        try:
+            self.__dict__[key]
+            prev = self.__dict__[key]["timestamp"]
+            curr = value["timestamp"]
+
+            period = round(curr - prev, 6)
+
+            # if period >= timedelta(seconds=5):
+            if period >= 5.10:
+                # RPT delayed: ViPAM5000PRT/LLN0$urcbA , 5.3140869140625
+                print(value["vmd-specific"], "delayed:", key, ",", period)
+
+                # r.lpush(key, period.total_seconds())  # lpush, lrange / array로 집어넣기
+                # print(r.lrange([i.decode() for i in delayed_ied][0], 0, -1))  / array 범위 가져오기
+                # r.expire(key, timedelta(15))
+
+                json_rpt = json.dumps({"name": key, "delay": period, "address": value["source"], "timestamp": curr},
+                                      ensure_ascii=False, ).encode("utf-8")
+                print(json_rpt)
+                r.set(value["vmd-specific"] + ":delay:" + key, json_rpt, 20)  #  20초 뒤에 만료/ period.total_seconds()
+            else:
+                print("packet arrived in", period)
+            print("################################################################")
+            # delayed_ied = [i.decode() for i in r.scan_iter("RPT:delay:*")]
+            # print(key, ", list: ", delayed_ied)
+        except KeyError as k:
+            print("Report key:", k, "not exist yet")
+            pass
+        except AttributeError as a:
+            print("AttributeError:", a)
+            pass
+        self.__dict__[key] = value
+
+
 class RingBuffer(deque):
     """
     inherits deque, pops the oldest data to make room
@@ -29,6 +68,7 @@ class RingBuffer(deque):
     def __init__(self, size):
         deque.__init__(self)
         self.size = size
+        print("## RingBuffer created ##")
 
     def full_append(self, item):
         deque.append(self, item)
@@ -47,21 +87,32 @@ class RingBuffer(deque):
 
 
 class LiveCapture:
-    def __init__(self, buff):
+    def __init__(self, buff, redis_connection):
         self.circular = buff
-        self.timeCheck = datetime.utcnow().strftime('%y-%m-%d/%H:%M:%S.%f')
+        self.rpt = Report(r)
+        self.r = redis_connection
+        # self.timeCheck = datetime.utcnow().strftime('%y-%m-%d/%H:%M:%S.%f')
+        # self.timeCheck = datetime.now()
+        self.timeCheck = time.time()
         self.cron = False
         self.request = []
         self.response = []
         # 패킷 캡쳐 실행
-        cap = pyshark.LiveCapture(interface='2', bpf_filter='tcp and port 102')  # GOOSE - ether proto 0x88B8
+        cap = pyshark.LiveCapture(interface='3', bpf_filter='tcp and port 102')  # GOOSE - ether proto 0x88B8
         # cap = pyshark.FileCapture('D:/dev/react-web-app/hmi_template/server/packet/mms/fresh.pcap', display_filter="mms or goose")
 
-        cap.apply_on_packets(self.save)  # 각 패킷마다 save 함수 시행
+        cap.apply_on_packets(self.save)  # 각 패킷마다 save 함수 시행   / callback, timeout, packet_count
+
+    def redisSetter(self, stamp, valueDict):
+        # redis setter - timestamp 를 key 값으로 packet 값을 dictionary 로 redis 에 저장
+        parse_dict = json.dumps(valueDict, ensure_ascii=False, ).encode("utf-8")  # redis 에 encoding 하여 저장
+        self.r.set("packet" + stamp, parse_dict)
 
     def save(self, pkt):
         # stamp = datetime.fromtimestamp(time.time()) # .strftime('%Y-%m-%d %H:%M:%S.%f') - default format
-        stamp = datetime.utcnow().strftime('%y-%m-%d/%H:%M:%S.%f')
+        # stamp = datetime.utcnow().strftime('%y-%m-%d/%H:%M:%S.%f')
+        # stamp = datetime.fromtimestamp(float(pkt.sniff_timestamp))
+        stamp = float(pkt.sniff_timestamp)
 
         try:
             mainDict = Dictlist()
@@ -84,21 +135,36 @@ class LiveCapture:
             # mainDict["key"] = mainArray[1]
             mainDict["timestamp"] = stamp
 
+            # dictionary 에 배열안의 key, value 값 할당 중복을 고려 value값을 배열로
             self.dictSetter("mms", mainDict, mainArray)
             self.dictSetter("eth", ethDict, ethArray)
 
             mainDict["eth"] = ethDict
 
             # print("Packet Arrived!")
-            print(mainDict)
+            # print(mainDict)
             # key, timestamp,. eth(Address[dest, src], Destination,  Source)
 
-            # print(pkt.mms)
+            # print(pkt)
 
             # redis setter
-            # parse_dict = json.dumps(mainDict, ensure_ascii=False, ).encode("utf-8")  # redis 에 encoding 하여 저장
-            # r.set("packet" + stamp, parse_dict)
+            # self.redisSetter(stamp, mainDict)
+
+            # circular 버퍼에 패킷 저장, 주기적으로 처리
             self.circular.append(mainDict)
+
+            # 각 ied report 발생주기 확인
+            try:
+                mainDict["vmd-specific"]
+                if mainDict["vmd-specific"][0] == "RPT":
+                    self.rpt[mainDict["visible-string"][0]] = {"vmd-specific": mainDict["vmd-specific"][0],
+                                                               "timestamp": stamp,
+                                                               "source": mainDict["eth"][0]["Source"][0]}
+                    # { "utc-time": mainDict["utc-time"][0],
+
+            except KeyError as k:
+                # print(k)
+                pass
 
             # 버퍼가 특정 수 이상 쌓이면 크론 실행
             if len(self.circular.get()) == 10:
@@ -118,8 +184,8 @@ class LiveCapture:
             #   1... .... = str1: True
             # 	.1.. .... = str2: True
         except AttributeError as e:
-            pass
             # print(e)
+            pass
 
     def categorizing(self):
         schedule = AsyncIOScheduler()
@@ -127,12 +193,15 @@ class LiveCapture:
         # 실행할 함수
         def exec_cron():
             time_check = self.timeCheck
-            self.timeCheck = datetime.utcnow().strftime('%y-%m-%d/%H:%M:%S.%f')
+            # self.timeCheck = datetime.utcnow().strftime('%y-%m-%d/%H:%M:%S.%f')
+            # self.timeCheck = datetime.now()
+            self.timeCheck = time.time()
             # len([pkt for pkt in self.circular if pkt["timestamp"][0] >= self.timeCheck]) #  쌓인 패킷 길이
+
             stacked_array = [pkt for pkt in self.circular if pkt["timestamp"][0] >= time_check]
             # print(self.timeCheck, len(stacked_array), stacked_array, )
 
-            # 패킷 분류 아래에 추가할 것!
+            # 패킷 분류 아래에 추가할 것! - request response 매칭
             # ied 연결 시작- initiate, confirmed, unconfirmed, ied 연결 종료 - conclude
             req = re.compile(r"(.+)-RequestPDU")
             res = re.compile(r"(.+)-ResponsePDU")
@@ -160,16 +229,21 @@ class LiveCapture:
                         """
                 # elif res.match(key):
                 #     res_list.append(key)
-            print("categorize on ", self.timeCheck)
+            # print("categorize on ", self.timeCheck)
 
             # 값이 남아있는 경우
             if len(compare_array) != 0:
                 print("Something remains... ", compare_array)
 
+        def rpt_cron():
+            print(self.rpt)
+
         # cron 으로 주기적으로 패킷 처리
         # .add_job(exec.add_job(exec_interval, 'interval', seconds=1)
-        schedule.add_job(exec_cron, 'cron', second="*")
+        schedule.add_job(exec_cron, 'cron', second="*")  # 매 초 분류
+        # schedule.add_job(rpt_cron, 'cron', second="*/5")
         schedule.start()  # 시작
+
         # exec_cron()
 
     @staticmethod
@@ -185,8 +259,6 @@ class LiveCapture:
                     if "invokeID" in data:
                         attr = data.split(": ")
                         diction[attr[0]] = attr[1]
-                    else:
-                        print("invokeID not exist:", data)
                 else:
                     if ": " in data:
                         pass
@@ -206,7 +278,7 @@ class LiveCapture:
 
 if __name__ == '__main__':
     # redis 연결
-    # r = redis.Redis(host="localhost", port=6379, db=0)
+    r = redis.Redis(host="localhost", port=6379, db=0)
 
     # circular 버퍼
     mms_circular = RingBuffer(250)
@@ -226,6 +298,6 @@ if __name__ == '__main__':
     # list = [{'key': pkt.key, 'timestamp': pkt.timestamp} for pkt in circular.get()]
     # print(list)
     # print(circular.get())
-    LiveCapture(mms_circular)
+    LiveCapture(mms_circular, r)
     # 최대 사이즈의 절반이상 이면 시작
     # 주기적으로 이전 마지막 timestamp 이후의 값들만 처리
